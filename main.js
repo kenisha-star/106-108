ee2015e82e33a02eef1bb5e3a3bf29003c7b29e1
kenisha-star <77574261+kenisha-star@users.noreply.github.com>
2021-07-02 10:16:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function capture() {
     document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
     });
 }
-classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/usnQD0v1Y/",modelloaded);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/usnQD0v1Y/model.json",modelloaded);
 function modelloaded() {
     console.log("model is loaded");
 }
@@ -24,25 +24,25 @@ function got_results(error,results) {
         console.log(results);
         document.getElementById("e_name").innerHTML=results[0].label;
         
-        document.getElementById("e2_name").innerHTML=results[0].label;
-        if (results[0].label == "happy") {
-          document.getElementById("e_emoji").innerHTML="&#128522;";
+        document.getElementById("e2_name").innerHTML=results[1].label;
+        if (results[0].label == "victory") {
+          document.getElementById("e_emoji").innerHTML="&#9996;";
         }
-        if (results[0].label == "sad") {
-          document.getElementById("e_emoji").innerHTML="&#128532;";
+        if (results[0].label == "amazing") {
+          document.getElementById("e_emoji").innerHTML="&#128076;";
         }
-        if (results[0].label == "angry") {
-          document.getElementById("e_emoji").innerHTML="&#128548;";
+        if (results[0].label == "best") {
+          document.getElementById("e_emoji").innerHTML="&#128077;";
         }
 
-        if (results[1].label == "happy") {
-          document.getElementById("e2_emoji").innerHTML="&#128522;";
+        if (results[1].label == "victory") {
+          document.getElementById("e2_emoji").innerHTML="&#9996;";
         }
-        if (results[1].label == "sad") {
-          document.getElementById("e2_emoji").innerHTML="&#128532;";
+        if (results[1].label == "amazing") {
+          document.getElementById("e2_emoji").innerHTML="&#128076;";
         }
-        if (results[1].label == "angry") {
-          document.getElementById("e2_emoji").innerHTML="&#128548;";
+        if (results[1].label == "best") {
+          document.getElementById("e2_emoji").innerHTML="&#128077;";
         }
     }
 }
